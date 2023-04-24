73cba8a14ad77577046ab62d7b024202880a840d
mohamedSharkawy1598 <124299658+mohamedSharkawy1598@users.noreply.github.com>
2023-04-24 22:49:13 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201284329186'] //ur owner number
+global.ownername = "Mohamed Al-Sharqawi ❤️" //ur owner name
+global.ytname = "YT: Mohamed Al-Sharqawi" //ur yt chanel name
+global.socialm = "GitHub: Mohamed Al-Sharqawi" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201284329186" //ur number
+global.premium = ['201284329186'] //ur premium number
+global.botname = '❤️ᴛᴏᴋɪᴛᴏ ʙᴏᴛ 𖢔❤️' //ur bot name
+global.linkz = "https://instagram.com/sharkawy721?igshid=ZDdkNTZiNTM=" //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/file/65c87042de6504b4443d1.jpg" //ur website to be displayed
+global.botscript = 'https://instagram.com/sharkawy721?igshid=ZDdkNTZiNTM=' //script link
 global.themeemoji = "♥" //ur theme emoji
 global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.author = "⍣201284329186⍣" //ur sticker watermark author
+global.wm = "❤️ᴛᴏᴋɪᴛᴏ ʙᴏᴛ 𖢔❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
